2967f1e8e6b09425aab45ef66dfc256557d26299
shake3deth <yuriyuri261215@gmail.com>
2026-07-12 00:59:54 +0900

    info完成

--- app/static/js/dep.js
+++ app/static/js/dep.js
@@ -2,9 +2,48 @@ const b_value = [
     "情報系",
     "機械",
     "生命・科学",
-    "建築",
+    "建築・デザイン",
     "まだ決まっていない",
 ]
+
+let score = []
+
+//[question,AJ,FI,EC]
+const info_value =[
+    ["CGやゲーム、音響、映像などを学びたい",1,5,4],
+    ["AIなどのデータサイエンスに興味がある",10,2,1],
+    ["コンピュータの技術やネットワークについて幅広く学びたい",3,1,3],
+    ["システムを一から作りたい",3,2,2],
+    ["サイバーセキュリティについて学びたい",3,2,2],
+    ["プログラミングなどの情報技術だけでなくハードウェア(物理的)についても学びたい",2,1,10],
+    ["通信システムについて実物を使って学びたい",3,1,3],
+    ["webデザインなどに興味がある",1,10,1],
+    ["もとからあるソフトなどを活用して制作を行いたい",1,3,1]
+];
+
+function total(value){
+    for(let i = 0 ; i < 3 ; i++){
+        let count = 0
+        for(let j = 0 ; j < value.length ; j++){
+            count += value[j][i+1];
+        }
+        score.push(count)
+    }
+    console.log(score);
+}
+
+
+const machine_value = [
+
+];
+
+const life_value = [
+
+];
+
+const arch_value = [];
+
+const none_value = [];
 
 window.onload = function(){
     let divv = document.getElementById("first");
@@ -55,11 +94,33 @@ function calculateScore(){
 function info(){
     let div = document.getElementById("second");
     div.textContent = "";
-    let div0 = document.createElement("div");
-    let h = document.createElement("h1");
-    h.textContent = "情報系";
-    div0.appendChild(h);
-    div.appendChild(div0);
+    for(let i = 0 ; i < info_value.length ; i++){
+        //問題文
+        let div0 = document.createElement("div");
+        div0.className = "question";
+        let h = document.createElement("h1");
+        h.textContent = `Q${i+2}. ${info_value[i][0]}`;
+        div0.appendChild(h);
+        div.appendChild(div0);
+        //問題の答え
+        let div1 = document.createElement("div");
+        div1.className = "center";
+        div1.id = `Q${i+2}`;
+        for(let j = 0 ; j < 2 ; j++){
+            let input = document.createElement("input");
+            input.type = "radio";
+            input.id = `Q${i+2}_${j+1}`;
+            input.name = `Q${i+2}`;
+
+            div1.appendChild(input);
+            let label = document.createElement("label");
+            label.htmlFor = `Q${i+2}_${j+1}`;
+            label.textContent = j==0 ? "はい" : "いいえ";
+            div1.appendChild(label);
+        }
+        div.appendChild(div1)
+    }
+    
 }
 
 function machine(){
